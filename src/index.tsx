f863f532b5e4643bdf9f27a5cc221ac3fd5f0ec7
Henrique Saias <henriquesaias@hsaias.com>
2023-10-14 19:17:03 +0100

    add chakra theme

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,14 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
---
+++ src/theme.tsx
@@ -0,0 +1,22 @@
+import { theme as chakraTheme } from '@chakra-ui/react'
+
+const fonts = { ...chakraTheme.fonts, mono: `'Menlo', monospace` }
+
+const breakpoints = ['40em', '52em', '64em']
+
+const theme = {
+  ...chakraTheme,
+  colors: {
+    ...chakraTheme.colors,
+    black: 'rgb(23,23,24)',
+    hoverColor: 'rgba(43,43,44, .5)'
+  },
+  fonts: {
+    heading: `'Unbounded', cursive`,
+    body: `'Hubballi', sans-serif`,
+    mono: "Menlo, monospace",
+  },
+  breakpoints
+}
+
+export default theme
