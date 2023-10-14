aa39b87ff1872d26fc38e2e512ae72894f659816
Henrique Saias <henriquesaias@hsaias.com>
2023-10-14 13:13:39 +0100

    basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='banners-container'>
+      <div id="banner-logo" className="home-banners" style={{ backgroundImage: 'url(https://i.postimg.cc/pr1hyG3Q/BANNER-1-min.jpg)' }}>
+        <img src="https://i.postimg.cc/jS8xTn0N/LOGO-BANNER-1.png" />
+      </div>
+      <div className="home-banners" style={{ backgroundImage: 'url(https://i.postimg.cc/3JV0TqXY/BANNER-2-min.jpg)' }}>
+        <img src="https://i.postimg.cc/yYHYyzyJ/FRASE-BANNER-2.png" />
+      </div>
+      <div className="home-banners" style={{ backgroundImage: 'url(https://i.postimg.cc/s29M5Dtg/BANNER-3-min.jpg)' }}>
+        <img src="https://i.postimg.cc/qvBJWXY4/FRASE-BANNER-3.png" />
+      </div>
+      <div className="home-banners" style={{ backgroundImage: 'url(https://i.postimg.cc/vmd1162K/BANNER-4-min.jpg)' }}>
+        <img src="https://i.postimg.cc/SNfQZtgg/FRASE-BANNER-4.png" />
+      </div>
     </div>
   );
 }
